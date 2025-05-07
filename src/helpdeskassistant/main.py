24e4b59e4e68a1255b17d96e9c91b1031b547f8a
maksnowak <59841779+maksnowak@users.noreply.github.com>
2025-05-08 01:59:04 +0200

    Add Dockerfile for frontend

--- src/helpdeskassistant/main.py
+++ src/helpdeskassistant/main.py
@@ -28,7 +28,7 @@ async def add_session(request: Request, call_next):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://localhost:4173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
