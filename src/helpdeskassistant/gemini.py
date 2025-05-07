c8d81d22bb1f018af34effd41e33833b653da510
maksnowak <59841779+maksnowak@users.noreply.github.com>
2025-05-07 15:46:29 +0200

    Fix chat resetting not working

--- src/helpdeskassistant/gemini.py
+++ src/helpdeskassistant/gemini.py
@@ -10,11 +10,7 @@ class GeminiClient:
 
         self.client = genai.Client(api_key=GEMINI_API_KEY)
         self.model = model
-
-        self.chat = self.client.chats.create(
-            model=self.model,
-            config=types.GenerateContentConfig(
-                system_instruction="""You are an IT helpdesk assistant at a company.
+        self.context = """You are an IT helpdesk assistant at a company.
 Your job is to help out users in filling out helpdesk forms when they have issues with their devices.
 Ask the following questions to get the form fields provided in parenthesis:
 1. What is their name (firstname and lastname, both max 20 characters; do not shorten anything, if the name is too long, truncate it),
@@ -31,9 +27,13 @@ After each response, return the following JSON object after an empty line:
     "urgency": int 
 }
 When the user provides all the information, say "Thank you for your input. Your request has been submitted." and end the conversation.
-""",
-            )
+"""
+
+        self.chat = self.client.chats.create(
+            model=self.model,
+            config=types.GenerateContentConfig(system_instruction=self.context)
         )
+
     def message(self, prompt):
         response = self.chat.send_message(prompt)
         return response.text
@@ -42,4 +42,7 @@ When the user provides all the information, say "Thank you for your input. Your 
         return self.chat.get_history()
     
     def reset_chat(self):
-        self.chat
+        self.chat = self.client.chats.create(
+            model=self.model,
+            config=types.GenerateContentConfig(system_instruction=self.context)
+        )
